9ebad53ae72af56ae539c4e1a8e6a30e277502fb
Mohammed Mahfoud <mhd.mahfoud@hotmail.com>
2024-03-13 15:33:09 +0300

    remove eliminate_multiples function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use stopwatch::Stopwatch;
 
-fn eliminate_multiples(a: &mut Vec<usize>, n: usize) -> &mut Vec<usize> {
-    a.retain(|&x| x <= n || x % n != 0);
-    a
+fn get_prime_numbers_less_than(max: u64) -> Vec<u64> {
+
+    // all numbers (before sifting) from 2 to max
+    let mut numbers: Vec<u64> = (2..=max).into_iter().collect();
+
+    // caculate where to stop
+    let square_root_of_max = (max as f64).sqrt();
+
+    // start sifting from 2 (i.e at index 0)
+    let mut i = 0;
+    while i < numbers.len() {
+        // this number is prime since it is not a multiple of any of the lesser primes
+        let &n = numbers.get(i).unwrap();
+
+        // break the loop when reaching a number that is bigger than the root of the upper limit, 
+        // because all its multiples were already removed.
+        if (n as f64) > square_root_of_max {
+            break;
+        }
+
+        // eliminate all multiples of n: retain other elements
+        numbers.retain(|&x| x <= n || x % n != 0);
+
+        // move to the next prime
+        i = i + 1;
+    }
+    numbers
 }
 
 fn main() {
-    const SIZE: usize = 10_000_000;
-    // all numbers (before sifting)
-    let mut numbers: Vec<usize> = (2..SIZE).into_iter().collect();
-    // start sifting from 2
-    let mut i = 0;
-    // start the timer
     let sw = Stopwatch::start_new();
-    while i < numbers.len() {
-        let n = numbers[i];
-        if (n as f64) > (SIZE as f64).sqrt() {
-            break;
-        }
-        numbers = eliminate_multiples(&mut numbers, n).to_vec();
-        //print!("{:?}\n====\n{:?}\n===============\n", n, numbers);
-        i = i + 1;
-    }
-    // stop the timer
+    let primes = get_prime_numbers_less_than(10_000_000);
     println!("{:?}", sw.elapsed());
-    println!("{:?}", numbers);
+    println!("{:?}", primes);
 }
