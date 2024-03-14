968d20c2581586c7fd47b83267150ee41d643ce2
Mohammed Mahfoud <mhd.mahfoud@hotmail.com>
2024-03-14 07:38:59 +0300

    changes by clippy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use stopwatch::Stopwatch;
 
 fn get_prime_numbers_less_than(max: u64) -> Vec<u64> {
-
     // all numbers (before sifting) from 2 to max
-    let mut numbers: Vec<u64> = (2..=max).into_iter().collect();
+    let mut numbers: Vec<u64> = (2..=max).collect();
 
     // caculate where to stop
     let square_root_of_max = (max as f64).sqrt();
@@ -14,7 +13,7 @@ fn get_prime_numbers_less_than(max: u64) -> Vec<u64> {
         // this number is prime since it is not a multiple of any of the lesser primes
         let &n = numbers.get(i).unwrap();
 
-        // break the loop when reaching a number that is bigger than the root of the upper limit, 
+        // break the loop when reaching a number that is bigger than the root of the upper limit,
         // because all its multiples were already removed.
         if (n as f64) > square_root_of_max {
             break;
@@ -24,14 +23,14 @@ fn get_prime_numbers_less_than(max: u64) -> Vec<u64> {
         numbers.retain(|&x| x <= n || x % n != 0);
 
         // move to the next prime
-        i = i + 1;
+        i += 1;
     }
     numbers
 }
 
 fn main() {
     let sw = Stopwatch::start_new();
-    let primes = get_prime_numbers_less_than(10_000_000);
+    let primes = get_prime_numbers_less_than(100);
     println!("{:?}", sw.elapsed());
     println!("{:?}", primes);
 }
